ccf50e5e91635fe705f27324273d4c59c2c9a1c5
HenriqueSerres <henriqueserres@gmail.com>
2022-08-31 14:20:24 -0300

    add exercicio 4

--- opCondicionais/Program.cs
+++ opCondicionais/Program.cs
@@ -1,8 +1,21 @@
-﻿int N = int.Parse(Console.ReadLine());
+﻿// exercicio 1
+/* int N = int.Parse(Console.ReadLine());
 
 if (N < 0) {
     Console.WriteLine("NEGATIVO");
 }
 else {
     Console.WriteLine("NAO NEGATIVO");
+} */
+
+//exercicio 3
+
+string[] entries = Console.ReadLine().Split(' '); 
+int A = int.Parse(entries[0]);
+int B = int.Parse(entries[1]);
+
+if (A % B == 0 || B % A == 0) {
+   Console.WriteLine("Divisível");
+} else {
+   Console.WriteLine("Não divisível");
 }
