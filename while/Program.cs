3ff5fccac57ef92574c936ff3c0dd596bfcec2f6
HenriqueSerres <henriqueserres@gmail.com>
2022-10-11 16:30:59 -0300

    estrutura while

---
+++ while/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+
+Console.Write("Digite um numero: ");
+double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+while (x >= 0.00)
+{
+    double raiz = Math.Sqrt(x);
+    Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+    Console.Write("Digite um numero: ");
+    x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+Console.WriteLine("Numero negativo!!");
+
+
+
